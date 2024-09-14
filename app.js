7b9cc6265dc93d9a6c88be190eaf6a2648daec87
uhammadziyo <DilafruzIminova71@gmail.com>
2024-09-14 10:41:40 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,67 @@
+// let body = document.querySelector(`body`)
+// let machine = document.createElement(`h1`)
+// machine.innerHTML = `welcome to camping adventures`
+// machine.style.color = `green`
+// body.appendChild(machine)
+// let machine2 = document.createElement(`p`)
+// machine2.innerHTML = `Experience the beauty of nature`
+// body.appendChild(machine2)
+// let machine3 = document.createElement(`p`)
+// machine3.innerHTML = `Empbark on unforgettable camping tips that`
+// body.appendChild(machine3)
+// let machineimg = document.createElement(`img`)
+// machineimg.src = `https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQZqbXPTcpxIiRXLD7vDf-54N7ZyiEF0_rDhQ&s`
+// let machine4 = document.createElement(`h2`)
+// machine4.innerHTML = `Popular Camping Destinations`
+// body.appendChild(machine4)
+// let machine5 = document.createElement(`ol`)
+// machine5.innerHTML = `mountain wilderness camp`
+// body.appendChild(machine5)
+
+// let machine6 = document.createElement(`ol`)
+// machine6.innerHTML = `lakeside retreat`
+// body.appendChild(machine6)
+// let machine7 = document.createElement(`ol`)
+// machine7.innerHTML = `coastal camping paradise`
+// body.appendChild(machine7)
+// let machine8 = document.createElement(`h3`)
+// machine8.innerHTML = `Upcoming Camping Event`
+// body.appendChild(machine8)
+// let machine9 = document.createElement(`ul`)
+// machine9.innerHTML = `Forest Camping Weekend`
+// body.appendChild(machine9)
+// let machine10 = document.createElement(`p`)
+// machine10.innerHTML = `Dates:August 20th -22nd`
+// body.appendChild(machine10)
+// let machine11 = document.createElement(`h4`)
+// machine11.innerHTML = `Testimonials`
+// body.appendChild(machine11)
+// let machine12 = document.createElement(`h5`)
+// machine12.innerHTML = `Sarah Smith`
+// body.appendChild(machine12)
+// let machine13 = document.createElement(`p`)
+// machine13.innerHTML = `"Camping Adventures offred the perfect escape from city life.The serene mountain views were breathtaking.`
+// body.appendChild(machine13)
+// let machine14 = document.createElement(`a`)
+// machine14.innerHTML = `Book YOur camping trip`
+// body.appendChild(machine14)
+
+let body = document.querySelector('body');
+
+body.style.display = 'flex'
+body.style.height = '100vh'
+body.style.justifyContent = 'center'
+body.style.alignItems = 'center'
+body.style.backgroundColor = prompt('bodyni rangini kirit hoshim')
+
+let textnirangi = document.createElement('h1')
+textnirangi.innerHTML = (
+
+    `28 апреля 2023 года подписал новый контракт с «Манчестер Юнайтед», рассчитанный до июня 2028 года[6]. 26 декабря 2023 года сделал «дубль» в матче Премьер-лиги против клуба «Астон Вилла», который завершился победой «Юнайтед» со счётом 3:2[7].
+
+25 мая 2024 года выиграл Кубок Англии, забив гол в финальном матче против «Манчестер Сити»[8].
+
+`)
+textnirangi.style.backgroundColor = prompt('textni orqa rangini kiriting')
+textnirangi.style.color = prompt('textni ozini rangini kirit qodirali')
+body.appendChild(textnirangi)
